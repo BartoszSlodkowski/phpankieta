fb06fa7e3eee4204e0e2a7f7ec094222a02bb383
Loczek <Minecraft1>
2019-01-23 11:06:41 +0100

    This is features

--- index.php
+++ index.php
@@ -71,6 +71,34 @@
 				  </label>
 				</div>
 			</div>
+			<div class="form-group">
+    			<label for="formGroupExampleInput">Example label:</label>
+ 			
+				<div class="form-check">
+				  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
+				  <label class="form-check-label" for="exampleRadios1">
+				    Default radio
+				  </label>
+				</div>
+				<div class="form-check">
+				  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios2" value="option2">
+				  <label class="form-check-label" for="exampleRadios2">
+				    Second default radio
+				  </label>
+				</div>
+				<div class="form-check">
+				  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios3" value="option3" >
+				  <label class="form-check-label" for="exampleRadios3">
+				    Default radio
+				  </label>
+				</div>
+				<div class="form-check">
+				  <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios4" value="option4" >
+				  <label class="form-check-label" for="exampleRadios4">
+				    Default radio
+				  </label>
+				</div>
+			</div>
 
 
 			<button type="submit" class="btn btn-primary">submit</button>
